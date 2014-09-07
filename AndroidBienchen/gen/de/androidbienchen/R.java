b1002b6b464473a3985aac441a801d663af4fd8a
BRIAS <matthias.braeuer@arcor.de>
2014-09-07 18:12:04 +0200

    first working navDrawer version is finished

--- AndroidBienchen/gen/de/androidbienchen/R.java
+++ AndroidBienchen/gen/de/androidbienchen/R.java
@@ -10,6 +10,11 @@ package de.androidbienchen;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int purple_dark=0x7f040002;
+        public static final int purple_light=0x7f040000;
+        public static final int purple_middle=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,15 +23,23 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int listitem_background=0x7f020001;
+        public static final int navdrawer_listselector=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int container=0x7f090000;
+        public static final int content_frame=0x7f090002;
+        public static final int drawer_layout=0x7f090001;
+        public static final int left_drawer=0x7f090003;
+        public static final int navmenuitem_icon=0x7f090004;
+        public static final int navmenuitem_label=0x7f090005;
+        public static final int navmenusection_label=0x7f090006;
     }
     public static final class layout {
         public static final int activity_calendar=0x7f030000;
@@ -43,26 +56,30 @@ public final class R {
         public static final int fragment_presence_status=0x7f03000b;
         public static final int fragment_scale=0x7f03000c;
         public static final int fragment_website_link=0x7f03000d;
+        public static final int navdrawer_item=0x7f03000e;
+        public static final int navdrawer_section=0x7f03000f;
     }
     public static final class menu {
-        public static final int calendar=0x7f070000;
-        public static final int cam=0x7f070001;
-        public static final int chat=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int presence_status=0x7f070004;
-        public static final int scale=0x7f070005;
-        public static final int website_link=0x7f070006;
+        public static final int calendar=0x7f080000;
+        public static final int cam=0x7f080001;
+        public static final int chat=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int presence_status=0x7f080004;
+        public static final int scale=0x7f080005;
+        public static final int website_link=0x7f080006;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_calendar=0x7f050008;
-        public static final int title_activity_cam=0x7f050004;
-        public static final int title_activity_chat=0x7f050006;
-        public static final int title_activity_presence_status=0x7f050007;
-        public static final int title_activity_scale=0x7f050003;
-        public static final int title_activity_website_link=0x7f050005;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int drawer_close=0x7f06000a;
+        public static final int drawer_open=0x7f060009;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_calendar=0x7f060008;
+        public static final int title_activity_cam=0x7f060004;
+        public static final int title_activity_chat=0x7f060006;
+        public static final int title_activity_presence_status=0x7f060007;
+        public static final int title_activity_scale=0x7f060003;
+        public static final int title_activity_website_link=0x7f060005;
     }
     public static final class style {
         /** 
@@ -86,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
