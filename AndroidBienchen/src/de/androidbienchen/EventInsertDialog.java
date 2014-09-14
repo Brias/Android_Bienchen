ae291b595d7bc399c129049125515a3b6f94d9c0
BRIAS <matthias.braeuer@arcor.de>
2014-09-14 19:58:33 +0200

    Revert """"
    This reverts commit cdfcfdc4518d7bc763a080ed17c55253e7097ee2.

--- AndroidBienchen/src/de/androidbienchen/EventInsertDialog.java
+++ AndroidBienchen/src/de/androidbienchen/EventInsertDialog.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
@@ -16,9 +17,11 @@ import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class EventInsertDialog {
 
