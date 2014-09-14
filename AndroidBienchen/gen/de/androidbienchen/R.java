4faff77408af786bafbec588354932e0415b12aa
BRIAS <matthias.braeuer@arcor.de>
2014-09-14 19:21:03 +0200

    ""

--- AndroidBienchen/gen/de/androidbienchen/R.java
+++ AndroidBienchen/gen/de/androidbienchen/R.java
@@ -11,9 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int purple_dark=0x7f040002;
-        public static final int purple_light=0x7f040000;
-        public static final int purple_middle=0x7f040001;
+        public static final int caldroid_black=0x7f040001;
+        public static final int caldroid_darker_gray=0x7f040005;
+        public static final int caldroid_gray=0x7f040004;
+        /**  A dark Holo shade of blue 
+         */
+        public static final int caldroid_holo_blue_dark=0x7f040007;
+        /**  A light Holo shade of blue 
+         */
+        public static final int caldroid_holo_blue_light=0x7f040006;
+        /**  For settings framework 
+         */
+        public static final int caldroid_lighter_gray=0x7f040003;
+        /**  A really bright Holo shade of blue 
+         */
+        public static final int caldroid_sky_blue=0x7f040008;
+        public static final int caldroid_transparent=0x7f040002;
+        public static final int caldroid_white=0x7f040000;
+        public static final int purple_dark=0x7f04000b;
+        public static final int purple_light=0x7f040009;
+        public static final int purple_middle=0x7f04000a;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -23,33 +40,52 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int listitem_background=0x7f020001;
-        public static final int navdrawer_listselector=0x7f020002;
+        public static final int calendar_next_arrow=0x7f020000;
+        public static final int calendar_prev_arrow=0x7f020001;
+        public static final int cell_bg=0x7f020002;
+        public static final int disable_cell=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int left_arrow=0x7f020005;
+        public static final int listitem_background=0x7f020006;
+        public static final int navdrawer_listselector=0x7f020007;
+        public static final int red_border=0x7f020008;
+        public static final int red_border_gray_bg=0x7f020009;
+        public static final int right_arrow=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f09000e;
+        public static final int calendar_gridview=0x7f09000a;
+        public static final int calendar_left_arrow=0x7f090004;
+        public static final int calendar_month_year_textview=0x7f090005;
+        public static final int calendar_right_arrow=0x7f090006;
+        public static final int calendar_title_view=0x7f090003;
+        public static final int calendar_tv=0x7f090009;
         public static final int content_frame=0x7f090001;
         public static final int drawer_layout=0x7f090000;
         public static final int left_drawer=0x7f090002;
-        public static final int navmenuitem_icon=0x7f090003;
-        public static final int navmenuitem_label=0x7f090004;
-        public static final int navmenusection_label=0x7f090005;
+        public static final int months_infinite_pager=0x7f090008;
+        public static final int navmenuitem_icon=0x7f09000b;
+        public static final int navmenuitem_label=0x7f09000c;
+        public static final int navmenusection_label=0x7f09000d;
+        public static final int weekday_gridview=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_calendar=0x7f030001;
-        public static final int fragment_cam=0x7f030002;
-        public static final int fragment_chat=0x7f030003;
-        public static final int fragment_presence_status=0x7f030004;
-        public static final int fragment_scale=0x7f030005;
-        public static final int fragment_website_link=0x7f030006;
-        public static final int navdrawer_item=0x7f030007;
-        public static final int navdrawer_section=0x7f030008;
+        public static final int calendar_view=0x7f030001;
+        public static final int date_cell=0x7f030002;
+        public static final int date_grid_fragment=0x7f030003;
+        public static final int fragment_calendar=0x7f030004;
+        public static final int fragment_cam=0x7f030005;
+        public static final int fragment_chat=0x7f030006;
+        public static final int fragment_presence_status=0x7f030007;
+        public static final int fragment_scale=0x7f030008;
+        public static final int fragment_website_link=0x7f030009;
+        public static final int navdrawer_item=0x7f03000a;
+        public static final int navdrawer_section=0x7f03000b;
     }
     public static final class menu {
         public static final int calendar=0x7f080000;
@@ -61,23 +97,23 @@ public final class R {
         public static final int website_link=0x7f080006;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int calendar=0x7f06000d;
-        public static final int cam=0x7f06000f;
-        public static final int chat=0x7f06000c;
-        public static final int drawer_close=0x7f06000a;
-        public static final int drawer_open=0x7f060009;
-        public static final int hello_world=0x7f060001;
-        public static final int presence=0x7f06000b;
-        public static final int statistic=0x7f06000e;
-        public static final int title_activity_calendar=0x7f060008;
-        public static final int title_activity_cam=0x7f060004;
-        public static final int title_activity_chat=0x7f060006;
-        public static final int title_activity_presence_status=0x7f060007;
-        public static final int title_activity_scale=0x7f060003;
-        public static final int title_activity_website_link=0x7f060005;
-        public static final int website=0x7f060010;
+        public static final int action_settings=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int calendar=0x7f05000d;
+        public static final int cam=0x7f05000f;
+        public static final int chat=0x7f05000c;
+        public static final int drawer_close=0x7f05000a;
+        public static final int drawer_open=0x7f050009;
+        public static final int hello_world=0x7f050001;
+        public static final int presence=0x7f05000b;
+        public static final int statistic=0x7f05000e;
+        public static final int title_activity_calendar=0x7f050008;
+        public static final int title_activity_cam=0x7f050004;
+        public static final int title_activity_chat=0x7f050006;
+        public static final int title_activity_presence_status=0x7f050007;
+        public static final int title_activity_scale=0x7f050003;
+        public static final int title_activity_website_link=0x7f050005;
+        public static final int website=0x7f050010;
     }
     public static final class style {
         /** 
@@ -100,11 +136,33 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
